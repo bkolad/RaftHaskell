193a1ade4548579667ecff27d925d929244b1849
bkolad <blazejkolad@gmail.com>
2016-11-20 23:22:53 +0100

    adding client

---
+++ src/Raft/Client.hs
@@ -0,0 +1,25 @@
+module Raft.Client where
+import Raft.Types
+
+import qualified System.Random as R
+
+
+clientService :: Peers -> Process()
+clientService ps@(Peers peers) = do
+    let pSize = length peers
+    r <- liftIO $ R.randomRIO (0, pSize -1 )-- :: Process Int
+    let peer = peers !! r
+    let x = 33 :: Int
+    send peer x
+    mA <- expectTimeout 1000 :: Process (Maybe Int)
+    case mA of
+        Nothing ->
+            clientService ps
+        Just a ->
+            client a
+
+
+
+
+
+client peer = undefined
